94932dde42fba7759905924ba7180a69eaa2cbed
Prathamesh Salunkhe <prathameshsalunkhe2006@gmail.com>
2025-01-31 10:45:29 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+import packing_logic
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/get_packing_list', methods=['POST'])
+def get_packing_list():
+    destination = request.form['destination']
+    date = request.form.get("date")
+    activities = request.form['activities'].split(',')  # Input as comma-separated
+    packing_list = packing_logic.generate_packing_list(destination, date, activities)
+    return render_template('result.html', packing_list=packing_list)
+
+if __name__ == '__main__':
+    app.run(debug=True)
